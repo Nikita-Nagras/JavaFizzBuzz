0300ec6590ff47419256f9714a85bea4d3ba37f8
Nikita Nagras <snbnikita@gmail.com>
2025-03-27 15:45:57 +0900

    fizzbuzz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        boolean fizz = num % 5 == 0;
+        boolean buzz = num % 3 == 0;
+
+        String result = (fizz ? "Fizz" : "") + (buzz ? "Buzz" : "");
+        System.out.println(result.isEmpty() ? num : result);
     }
 }
